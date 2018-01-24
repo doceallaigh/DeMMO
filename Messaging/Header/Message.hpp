beb85398b0d578fca920568dea40f6ee915e84fd
Unknown <dylan.oceallaigh@gmail.com>
2018-01-23 21:08:10 -0500

    Added Payload to Message type
    Changed Message to a parameterized POD object, added the payload as a field.  This may be revised later.

--- Messaging/Header/Message.hpp
+++ Messaging/Header/Message.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #pragma region Library Includes
-#include <string>
 #include <memory>
+#include <string>
 #pragma endregion
 
 #pragma region Local Includes
@@ -16,78 +16,18 @@ typedef unsigned long messageId_t;
 #pragma endregion
 
 /*! \brief Describes the basic unit by which modules trigger work for each other
-*
-* \remarks Promise(s):
-*     1. The implementation provides access to an unchanging topic, which allows it to be used by the MessageBus to route it to its destination(s)
-*     2. The implementation provides access to an unchanging identifier.  This identifier is presumed to be unique, but this is not programatically enforced.
 * */
-class Message 
+template <typename TPayload>
+struct Message
 {
-#pragma region Class Assertions
-#pragma endregion
-
 public:
-#pragma region Operators
-    //! \cond \brief Copy assignment operator \endcond
-    // Message& operator= (const Message &original);
+	//! \brief The message topic, used for the purposes of routing and filtering
+	std::string Topic;
 
-    //! \cond \brief Move assignment operator \endcond
-    // Message& operator= (Message &&original) noexcept;
-#pragma endregion
+	//! \brief The message ID, intended to be a unique identifier among message objects
+	messageId_t Id;
 
-#pragma region Custom Constructors
-    /*! \cond \brief <Brief description goes here> \endcond
-    *
-    * \cond \param[in] <Parameter description goes here> \endcond
-    * */
-    // Message (T ... args);
-#pragma endregion
-
-#pragma region Standard Constructors & Destructor
-    //! \brief Default Constructor
-    Message(void) = default;
-
-    //! \brief Copy Constructor
-    Message(const Message &original) = default;
-
-    //! \brief Move Constructor
-    // Message(const Message &&original) noexcept = default;
-
-    //! \brief Destructor
-    virtual ~Message(void) noexcept = default;
-#pragma endregion
-
-private:
-#pragma region Private Constructors
-    //! \cond \brief Private Default Constructor \endcond
-    // Message(void) = default;
-#pragma endregion
-
-public:
-#pragma region Public Methods
-	// TODO Consider making this class A POD
-	const std::string GetTopic(void) const;
-
-	const messageId_t GetId(void) const;
-#pragma endregion
-
-protected:
-#pragma region Protected Methods
-#pragma endregion
-
-private:
-#pragma region Private Methods
-#pragma endregion
-
-public:
-#pragma region Public Fields
-#pragma endregion
-
-protected:
-#pragma region Protected Fields
-#pragma endregion
-
-private:
-#pragma region Private Fields
-#pragma endregion
+	// TODO Evaluate whether or not this should be cached externally.
+	//! \brief The message payload, which allows the consumer to act on the message
+	std::shared_ptr<const TPayload> Payload;
 };
