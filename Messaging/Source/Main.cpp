b94e5c90e71478f98af5e93775432377069af058
Unknown <dylan.oceallaigh@gmail.com>
2018-01-24 22:44:22 -0500

    Added test code in Messaging Main
    Added a small snippet to test MessageRouter in Messaging Main file

--- Messaging/Source/Main.cpp
+++ Messaging/Source/Main.cpp
@@ -1,3 +1,9 @@
+#include "../Header/MessageRouter.hpp"
+#include "../Header/SubscriptionMap.hpp"
+#include "../Header/Message.hpp"
+
 int main(int numArgs, char * args[])
 {
+	MessageRouter messageRouter(std::make_unique<SubscriptionMap>());
+	messageRouter.Publish(std::make_shared<Message<int>>(Message<int>()));
 }
