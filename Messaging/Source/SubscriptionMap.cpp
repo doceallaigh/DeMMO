81a96ad5ad5b3b3906f9c33bf77d262c288cff52
Unknown <dylan.oceallaigh@gmail.com>
2018-01-24 22:42:33 -0500

    Added SubscriptionMap implementation
    Added (nothing) implementation for SubscriptionMap for linking purposes

---
+++ Messaging/Source/SubscriptionMap.cpp
@@ -0,0 +1,43 @@
+#pragma region Library Includes
+#pragma endregion
+
+#pragma region Local Includes
+#include "../Header/SubscriptionMap.hpp"
+#pragma endregion
+
+#pragma region Constants
+#pragma endregion
+
+#pragma region Static Fields
+#pragma endregion
+
+#pragma region Operators
+#pragma endregion
+
+#pragma region Custom Constructors
+#pragma endregion
+
+#pragma region Standard Constructors & Destructor
+#pragma endregion
+
+#pragma region Private Constructors
+#pragma endregion
+
+#pragma region Public Methods
+void SubscriptionMap::TrackSubscriptions(std::shared_ptr<SubscriptionCache> &subscriptionCache, unsigned long subscriberId)
+{
+	// TODO Implement
+}
+
+std::vector<unsigned long> SubscriptionMap::GetSubscriberIds(std::string topic)
+{
+	// TODO Implement
+	return std::move(std::vector<unsigned long>());
+}
+#pragma endregion
+
+#pragma region Protected Methods
+#pragma endregion
+
+#pragma region Private Methods
+#pragma endregion
